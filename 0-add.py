9e58a60a5a83b3322bd72fc7e6b2f474d5dfa3fa
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2023-04-28 20:06:04 +0100

    Create 0-add.py

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+This module contains the function `add`
+"""
+def add(a: float, b: float) -> float:
+    """Return the sum of two float arguments as a float"""
+    return a + b
