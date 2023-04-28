75b9f20074002e976fb96ab2122f2ea44b4ee3fe
jojobutercup <jojobutercup@gmail.com>
2023-04-28 02:26:36 -0700

    commit

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,12 @@
 def add(a: float, b: float) -> float:
+    """
+    Return the sum of two numbers.
+    
+    Arguments:
+    a -- a float number.
+    b -- a float number.
+    
+    Returns:
+    The sum of a and b as a float.
+    """
     return a + b
