668b0f06df9dd45d8cdc4e61cafa99233e306bb1
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2023-04-29 02:07:14 +0100

    Delete 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-This module contains the function `add`
-"""
-def add(a: float, b: float) -> float:
-    """Return the sum of two float arguments as a float"""
-    return a + b
