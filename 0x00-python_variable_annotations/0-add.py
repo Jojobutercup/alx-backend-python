c9ab372ce2993e8a7b17258358760d4161ea9e64
jojobutercup <jojobutercup@gmail.com>
2023-04-29 00:33:47 +0000

    commit

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module contains the function `add`
+"""
 
 
 def add(a: float, b: float) -> float:
-    """
-    Return the sum of two numbers.
-
-    Args:
-    a: A float number.
-    b: A float number.
-
-    Returns:
-    The sum of a and b as a float.
-    """
+    """Return the sum of two float arguments as a float"""
     return a + b
