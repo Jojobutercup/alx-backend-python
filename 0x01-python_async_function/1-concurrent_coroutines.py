04ad139ab0c4e8a6f0bac531f8b8ce1c1e7e63ea
jojobutercup <jojobutercup@gmail.com>
2023-05-03 16:16:42 -0700

    commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,17 +5,27 @@ from typing import List
 from random import uniform
 
 
+"""
+Module: 1-concurrent_coroutines
+------------------------
+
+This module contains two asynchronous coroutines:
+wait_random and wait_n.
+
+"""
+
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for a
-    random delay between 0 and max_delay seconds (included)
-    and eventually returns it.
+    random delay between 0 and max_delay seconds (inclusive)
+    and returns it.
 
     Args:
-    - max_delay (int): maximum delay in seconds (default 10)
+        max_delay (int): the maximum delay time in seconds (default 10)
 
     Returns:
-    - float: random delay between 0 and max_delay seconds (included)
+        float: a random delay time between 0 and max_delay seconds (inclusive)
     """
     delay = uniform(0, max_delay)
     await asyncio.sleep(delay)
@@ -24,17 +34,17 @@ async def wait_random(max_delay: int = 10) -> float:
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous coroutine that spawns
-    n instances of the wait_random coroutine with the specified max_delay
+    Asynchronous coroutine that spawns n instances of
+    the wait_random coroutine with the specified max_delay
     and returns a list of all the resulting delays in ascending order.
 
     Args:
-    - n (int): number of times to spawn wait_random
-    - max_delay (int): maximum delay
-    in seconds to pass to each instance of wait_random
+        n (int): the number of times to spawn wait_random
+        max_delay (int): the maximum delay time in
+        seconds to pass to each instance of wait_random
 
     Returns:
-    - List[float]: list of delays in ascending order
+        List[float]: a list of delays in ascending order
     """
     delays = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
     return sorted(delays)
