6de77bbb99db2a2461b9ba25782f0dc65761b238
jojobutercup <jojobutercup@gmail.com>
2023-05-05 15:22:23 -0700

    commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ that waits for a random delay
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that yields 10 random numbers
     between 0 and 10, waiting for 1 second between each yield.
